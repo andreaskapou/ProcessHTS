85c73ef45b5492f830990d7df37bd53283b0d5e3
andreaskapou <kapouranis.andreas@gmail.com>
2016-01-29 19:31:37 +0000

    Added funvtions for removing chromosomes, removing noisy reads and extracting gene names

--- R/utils.R
+++ R/utils.R
@@ -53,3 +53,67 @@ extract_fpkm <- function(x){
   fpkm <- gsub(".* FPKM ([^;]+);.*", "\\1", x)
   return(as.numeric(fpkm))
 }
+
+
+#' Extract gene name from string
+#'
+#' \code{extract_gene_name} Extracts gene name from a string
+#'
+#' @param x a string containing gene name information
+#'
+#' @return The gene name as a string
+#'
+#' @examples
+#' data <- 'gene_name "Bnt1.1"; transcr "ENST00000456328"; FPKM "0.0736831";'
+#' scaled <- extract_gene_name(data)
+#'
+#' @export
+extract_gene_name <- function(x){
+  # TODO test when no gene name is available
+  gene_name <- gsub(".* gene_name ([^;]+);.*", "\\1", x)
+  return(gene_name)
+}
+
+
+#' Discard selected chromosomes
+#'
+#' \code{discard_chr} Discards selected chromosomes
+#'
+#' @param x The HTS data stored in a data frame object
+#' @param chr_discarded A vector with chromosome names to be discarded.
+#'
+#' @return The reduced HTS data.
+#'
+#' @export
+discard_chr <- function(x, chr_discarded = NULL){
+  if (!is.null(chr_discarded)){
+    message("Removing selected chromosomes ...")
+    for (i in 1:length(chr_discarded)){
+      x <- subset(x, x$chr != chr_discarded[i])
+    }
+  }
+  return(x)
+}
+
+
+#' Discard BS-Seq noisy reads
+#'
+#' \code{discard_bs_noise_reads} discards low coverage and (really) high reads
+#'  from BS-Seq experiments. These reads can be thought as noise of the
+#'  experiment.
+#'
+#' @param bs_data A Granges object containing the BS-Seq data.
+#' @param min_bs_cov The minimum number of reads mapping to each CpG site.
+#' @param max_bs_cov The maximum number of reads mapping to each CpG site.
+#'
+#' @return The reduced Granges object without noisy observations
+#'
+#' @export
+discard_bs_noise_reads <- function(bs_data, min_bs_cov = 2, max_bs_cov = 1000){
+
+  message("Discarding noisy reads ...")
+  bs_data <- subset(bs_data,
+                    bs_data$total_reads > min_bs_cov &
+                    bs_data$total_reads < max_bs_cov)
+  return(bs_data)
+}
