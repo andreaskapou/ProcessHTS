bfe2b2c8832b7f521329a3040cea8e4e7f73a395
andreaskapou <kapouranis.andreas@gmail.com>
2016-01-29 19:29:33 +0000

    Pool replicate data from ENCODE HAIB BS-Seq data

---
+++ R/pool_bs_encode_haib_rep.R
@@ -0,0 +1,60 @@
+#' Read and pool replicates from Bismark Cov formatted BS-Seq data
+#'
+#' \code{pool_bs_bismark_cov_rep} reads and pools replicate methylation data
+#'  from  BS-Seq experiments that are in Bismark cov format.
+#'
+#' @inheritParams preprocess_bs_bismark_cov
+#'
+#' @return A \code{\link[GenomicRanges]{GRanges}} object.
+#'
+#' @seealso \code{\link{read_bs_bismark_cov}},
+#'  \code{\link{preprocess_bs_bismark_cov}}
+#'
+#' @examples
+#' bs_file1 <- system.file("extdata", "rrbs.bed", package = "processHTS")
+#' bs_file2 <- system.file("extdata", "rrbs.bed", package = "processHTS")
+#' bs_files <- c(bs_file1, bs_file2)
+#' pool_data <- pool_bs_encode_haib_rep(files=bs_files)
+#'
+#' @export
+pool_bs_encode_haib_rep <- function(files, chr_discarded = NULL){
+  message("Pooling BS-Seq replicates ...")
+
+  # Read first file
+  pooled_bs <- read_bs_encode_haib(file          = files[1],
+                                   chr_discarded = chr_discarded,
+                                   is_GRanges    = TRUE)
+  for (i in 2:length(files)){
+    # Read replicate file i
+    bs_data_rep <- read_bs_encode_haib(file          = files[i],
+                                       chr_discarded = chr_discarded,
+                                       is_GRanges    = TRUE)
+
+    # Find overlaps between BS-Seq replicates.
+    # A Hits object containing in the 1st column the query indices and in
+    # the 2nd column the corresponding subject indices that overlap.
+    overlaps <- GenomicRanges::findOverlaps(query   = pooled_bs,
+                                            subject = bs_data_rep)
+
+    # Get only the subset of overlapping CpG sites
+    tmp_bs <- pooled_bs[S4Vectors::queryHits(overlaps)]
+
+    # Add the total reads from the two distinct replicates
+    tmp_bs$total_reads <- tmp_bs$total_reads +
+                  bs_data_rep[S4Vectors::subjectHits(overlaps)]$total_reads
+
+    # Add the methylated reads from the two distinct replicates
+    tmp_bs$meth_reads <- tmp_bs$meth_reads +
+                  bs_data_rep[S4Vectors::subjectHits(overlaps)]$meth_reads
+
+    # Add CpG sites that were not overlapping between replicates
+    tmp_bs <- c(tmp_bs,
+                pooled_bs[-S4Vectors::queryHits(overlaps)],
+                bs_data_rep[-S4Vectors::subjectHits(overlaps)])
+
+    # Sort the pooled GRanges object
+    pooled_bs <- sort(tmp_bs, ignore.strand=TRUE)
+  }
+  message("Finished pooling BS-Seq replicates!\n")
+  return(pooled_bs)
+}
