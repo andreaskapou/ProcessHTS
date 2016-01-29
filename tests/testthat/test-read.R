ee56710234d81920d089bc1b921993aeea30bc8b
andreaskapou <kapouranis.andreas@gmail.com>
2016-01-29 19:31:02 +0000

    Update tests accordingly since function names changed

--- tests/testthat/test-read.R
+++ tests/testthat/test-read.R
@@ -3,13 +3,13 @@ context("Reading files")
 rnaseq_file <- "rnaseq.bed"
 
 test_that("erros are thrown", {
-  expect_error(read.chrom_size("wrong_file_name"))
+  expect_error(read_chrom_size("wrong_file_name"))
 })
 
 test_that("returns required output", {
-  out <- read.rnaseq(rnaseq_file, is_list=TRUE)
-  expect_is(out, "list")
-
-  out <- read.rnaseq(rnaseq_file, is_list=FALSE)
+  out <- read_rna_encode_caltech(rnaseq_file, is_GRanges=TRUE)
   expect_is(out, "GRanges")
+
+  out <- read_rna_encode_caltech(rnaseq_file, is_GRanges=FALSE)
+  expect_is(out, "data.frame")
 })
