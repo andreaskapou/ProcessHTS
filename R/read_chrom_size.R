6ebdd90e2ad87f14c72f6ee0474e461c8867670c
andreaskapou <kapouranis.andreas@gmail.com>
2016-02-05 19:31:55 +0000

    Read chromosome size file using fread

--- R/read_chrom_size.R
+++ R/read_chrom_size.R
@@ -1,18 +1,19 @@
 #' Read file containing genome chromosome sizes.
 #'
-#' \code{read_chrom_size} reads a file containing genome chromosome sizes
-#' using the \code{\link{scan}} function.
+#' \code{read_chrom_size} reads a file containing genome chromosome sizes using
+#'  the \code{\link{scan}} function.
 #'
 #' @param file The name of the file to read data values from.
 #'
-#' @return data frame.
+#' @return A \code{\link[data.table]{data.table}} object.
 #'
-#' @seealso \code{\link{read_rna_encode_caltech}}, \code{\link{read_bs_encode_haib}}
+#' @seealso \code{\link{read_rna_encode_caltech}},
+#'  \code{\link{read_bs_encode_haib}}
 #'
 #' @examples
 #' # Get the location of the hg19 file
 #' hg19_file <- system.file("extdata", "hg19", package = "processHTS")
-#' data <- read_chrom_size(file=hg19_file)
+#' hg_data <- read_chrom_size(file=hg19_file)
 #'
 #' \dontrun{
 #' read.chrom_size(20)
@@ -21,16 +22,11 @@
 #' @export
 read_chrom_size <- function(file){
   message("Reading file ", file, " ...")
-  data_raw <- scan(file=file,
-                   sep="\t",
-                   what=list("character",  # Reference chromosome
-                             integer()     # Chromosome size
-                   ))
-
-  # Create a data frame with the chromosome sizes
-  data <- as.data.frame(data_raw[[2]], row.names=data_raw[[1]])
-  colnames(data) <- "size"
+  data_raw <- data.table::fread(input = file,
+                               sep = "\t",
+                               header = FALSE,
+                               col.names = c("chr", "size"))
 
   message("Done!\n")
-  return(data)
+  return(data_raw)
 }
