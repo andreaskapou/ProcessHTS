593f222b3503f79d32060164b9aaa874cba7d812
andreaskapou <kapouranis.andreas@gmail.com>
2016-02-05 19:32:11 +0000

    Read RNA-Seq file using fread

--- R/read_rna_beatson.R
+++ R/read_rna_beatson.R
@@ -2,31 +2,29 @@
 #'
 #' \code{read_rna_beatson} reads a file containing promoter annotation
 #' data together with gene expression levels from RNA-Seq experiments using the
-#' \code{\link{scan}} function.
+#' \code{\link[data.table]{fread}} function.
 #'
 #' @inheritParams read_bs_encode_haib
 #'
 #' @return a \code{\link[GenomicRanges]{GRanges}} object if \code{is_GRanges}
-#'  is TRUE, otherwise a data.frame object.
+#'  is TRUE, otherwise a \code{\link[data.table]{data.table}} object.
 #'
 #' @seealso \code{\link{read_annot_beatson}}, \code{\link{read_bs_bismark_cov}}
 #'
 #' @examples
 #' # Get the location of the RNA-Seq file
 #' rna_beatson_file <- system.file("extdata", "rna_beatson.bed", package = "processHTS")
-#' rna_beatson_data <- read_rna_beatson(file=rna_beatson_file, is_GRanges=TRUE)
+#' rna_beatson_data <- read_rna_beatson(file=rna_beatson_file, chr_discarded = "chrX", is_GRanges=TRUE)
 #'
 #' @export
-read_rna_beatson <- function(file, chr_discarded = NULL,
-                                    is_GRanges = TRUE){
+read_rna_beatson <- function(file, chr_discarded = NULL, is_GRanges = TRUE){
   message("Reading file ", file, " ...")
-  rna_data <- read.table(file = file,
-                         header = TRUE,
-                         sep = "\t",
-                         col.names = c("chr", "start", "end", "strand",
-                                       "gene_name", "ensembl_id", "gene_fpkm"),
-                         comment.char = "",
-                         stringsAsFactors = FALSE)
+  rna_data <- data.table::fread(input = file,
+                                sep = "\t",
+                                header = TRUE,
+                                col.names = c("chr", "start", "end", "strand",
+                                              "gene_name", "ensembl_id",
+                                              "gene_fpkm"))
 
 
   # Remove selected chromosomes  -------------------------------
@@ -36,12 +34,7 @@ read_rna_beatson <- function(file, chr_discarded = NULL,
   # Sorting data -----------------------------------------------
   # With order priority: 1. chr, 2. start, 3. strand
   message("Sorting RNA-Seq data ...")
-  rna_data <- rna_data[with(rna_data, order(rna_data$chr,
-                                            rna_data$start,
-                                            rna_data$strand)), ]
-
-  # Get sequential row numbers
-  row.names(rna_data) <- NULL
+  rna_data <- rna_data[order(rna_data$chr, rna_data$start, rna_data$strand)]
 
 
   if (is_GRanges){
