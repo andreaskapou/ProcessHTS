3ae400695ab22fe6d9bc13f55674493cc88f1d8a
andreaskapou <kapouranis.andreas@gmail.com>
2016-05-12 16:12:38 +0100

    Update function for reading chromosome sizes file

--- R/read_chrom_size.R
+++ R/read_chrom_size.R
@@ -1,4 +1,4 @@
-#' Read file containing genome chromosome sizes.
+#' Read genome chromosome sizes file.
 #'
 #' \code{read_chrom_size} reads a file containing genome chromosome sizes using
 #'  the \code{\link[data.table]{fread}} function.
@@ -6,6 +6,8 @@
 #' @param file The name of the file to read data values from.
 #'
 #' @return A \code{\link[data.table]{data.table}} object.
+#'
+#' @author C.A.Kapourani \email{C.A.Kapourani@@ed.ac.uk}
 #'
 #' @seealso \code{\link{read_rna_encode_caltech}},
 #'  \code{\link{read_bs_encode_haib}}
